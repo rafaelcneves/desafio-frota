86c23e847e4a61466f20420b8472a73ac75ab5d9
Rafael César Neves <rafael.ceesar@gmail.com>
2015-04-24 19:59:49 -0300

    first commit

---
+++ js/frotas/app.js
@@ -0,0 +1,34 @@
+(function(){
+  var app = angular.module("frota", []);
+
+  app.controller("FrotaController", function(){
+    this.products = frota;
+  });
+
+  var frota = [
+    {
+      "combustivel": "Flex",
+      "imagem": null,
+      "marca": "Volkswagem",
+      "modelo": "Gol",
+      "placa": "FFF­5498",
+      "cor": "Vermelho"
+    },
+    {
+      "combustivel": "Gasolina",
+      "imagem": null,
+      "marca": "Volkswagem",
+      "modelo": "Fox",
+      "placa": "FOX­4125",
+      "cor": null
+    },
+    {
+      "combustivel": "Alcool",
+      "imagem": "http://img.estadao.com.br/wp/jornal-do-carro/files/2012/08/FUSCA-1.jpg",
+      "marca": "Volkswagen",
+      "modelo": "Fusca",
+      "placa": "PAI­4121",
+      "cor": "Azul"
+    }
+  ]
+})();
