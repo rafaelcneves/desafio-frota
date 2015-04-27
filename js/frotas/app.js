b0c4f5f53dce233449f8afddd6f31f79d728d4ba
Rafael César Neves <rafael.ceesar@gmail.com>
2015-04-26 22:30:56 -0300

    corrigido base

--- js/frotas/app.js
+++ js/frotas/app.js
@@ -78,54 +78,6 @@
       "cor": null
     },
     {
-      "combustivel": "Flex",
-      "imagem": null,
-      "marca": "Volkswagem",
-      "modelo": "Gol",
-      "placa": "FFF­5498",
-      "cor": "Vermelho"
-    },
-    {
-      "combustivel": "Gasolina",
-      "imagem": null,
-      "marca": "Volkswagem",
-      "modelo": "Fox",
-      "placa": "FOX­4125",
-      "cor": null
-    },
-    {
-      "combustivel": "Flex",
-      "imagem": null,
-      "marca": "Volkswagem",
-      "modelo": "Gol",
-      "placa": "FFF­5498",
-      "cor": "Vermelho"
-    },
-    {
-      "combustivel": "Gasolina",
-      "imagem": null,
-      "marca": "Volkswagem",
-      "modelo": "Fox",
-      "placa": "FOX­4125",
-      "cor": null
-    },
-    {
-      "combustivel": "Flex",
-      "imagem": null,
-      "marca": "Volkswagem",
-      "modelo": "Gol",
-      "placa": "FFF­5498",
-      "cor": "Vermelho"
-    },
-    {
-      "combustivel": "Gasolina",
-      "imagem": null,
-      "marca": "Volkswagem",
-      "modelo": "Fox",
-      "placa": "FOX­4125",
-      "cor": null
-    },
-    {
       "combustivel": "Alcool",
       "imagem": "http://img.estadao.com.br/wp/jornal-do-carro/files/2012/08/FUSCA-1.jpg",
       "marca": "Volkswagen",
